31a172694cb92ab81fffe6584902d338210c5e51
ValentinaLujan12 <159487000+ValentinaLujan12@users.noreply.github.com>
2024-02-16 17:04:08 -0500

    prueba 1

---
+++ src/test/java/test/Asiento.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Asiento {
+	static String[] colores = {"rojo", "verde", "amarillo", "negro", "blanco"};
+	
+	String color;
+	int precio;
+	int registro;
+	
+	void cambiarColor(String color) {
+        for (String c : colores) {
+            if (c.equals(color)) {
+                this.color = color;
+                break;
+            }
+        }
+    }
+}
